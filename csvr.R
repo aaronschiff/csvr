454caced25e4c58dfe45cbcfb3de2eaa9647507c
Aaron Schiff <aaron@schiff.co.nz>
2016-01-15 07:15:09 +1300

    Started on countCategorical function

--- csvr.R
+++ csvr.R
@@ -1,3 +1,14 @@
+# Count unique levels of each categorical variable
+countCategorical = function(d) {
+  for (n in names(d)) {
+    if (typeof(d[[n]]) == "character") {
+      print(n)
+    }
+  }
+}
+
+
+# Main analysis function
 csvr = function(csvFile, ...) {
   # Setup
   library(dplyr)
@@ -5,6 +16,12 @@ csvr = function(csvFile, ...) {
   
   # Read data
   if (!missing(csvFile)) {
-    csvData = read.csv(csvFile, ...)
+    csvData = read.csv(csvFile, stringsAsFactors = FALSE, ...)
+    
+    if (nrow(csvData) > 0) {
+      counts = countCategorical(csvData)
+    }
   }
 }
+
+csvr("gdp.csv")
